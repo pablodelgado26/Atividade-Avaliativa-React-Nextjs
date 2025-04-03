89a923052e4d5fe1f261c415f4a270e8b170dc18
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-04-03 13:41:01 -0300

    feat: criação de componente footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
+import Footer from "./components/footer";
 import Header from "./components/header";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
+      <Footer />
     </div>
   );
 }
